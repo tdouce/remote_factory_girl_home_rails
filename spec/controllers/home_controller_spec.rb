630acda450ca929779987da21bedd4db43f3b0bd
tdouce <travisdouce@gmail.com>
2014-04-04 10:58:58 -0400

    Reorganizes specs

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -11,23 +11,31 @@ describe RemoteFactoryGirlHomeRails::HomeController do
   routes { RemoteFactoryGirlHomeRails::Engine.routes }
 
   describe '#create' do
-    it 'should skip :authentication and :another_authentication methods defined in ApplicationController' do
-      FactoryGirl.stub(:create)
-      post :create, {:factory => 'user', :attributes => {:first_name => 'Sam', :last_name  => 'Iam'}}
-      expect(response).to_not redirect_to("/401.html")
+    describe "when '.skip_before_filter' methods are configured" do
+      it 'should skip :authentication and :another_authentication methods defined in ApplicationController' do
+        post :create, {'factory' => 'user', :attributes => {'first_name' => 'Sam'}}
+        expect(response).to_not redirect_to('/401.html')
+      end
+    end
+
+    describe 'when enabled' do
+
+      before { RemoteFactoryGirlHomeRails.enable! }
+
+      it 'should return status code 200' do
+        post :create, {'factory' => 'user'}
+        expect(response.status).to eq(200)
+      end
+
+      it 'should create a factory' do
+        expect(FactoryGirl).to receive(:create).with(:user, {})
+        post :create, {'factory' => 'user'}
+      end
     end
 
     it 'should return status code 403 when RemoteFactoryGirlHomeRails is not enabled' do
       post :create, {}
       expect(response.status).to eq(403)
-    end
-
-    it 'should return status code 200 when RemoteFactoryGirlHomeRails is enabled' do
-      controller.stub(:factory)
-      FactoryGirl.stub(:create)
-      RemoteFactoryGirlHomeRails.enable!
-      post :create, {}
-      expect(response.status).to eq(200)
     end
   end
 end
