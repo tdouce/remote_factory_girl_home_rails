d8801abdfbd04e49f4a1792418ebd42bad2394a0
tdouce <travisdouce@gmail.com>
2014-11-13 16:59:55 -0500

    Use spec helper to parse json and compare key/values

--- spec/features/returning_all_defined_factoried_spec.rb
+++ spec/features/returning_all_defined_factoried_spec.rb
@@ -10,6 +10,6 @@ describe 'Returning all defined factories' do
 
   it 'should return a list of available factories' do
     get remote_factory_girl_home_rails.home_index_path
-    expect(response.body).to eq('{"factories":["user"]}')
+    expect(response_json(response.body)['factories']).to eq(['user'])
   end
 end
