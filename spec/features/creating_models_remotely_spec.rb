81b07915aae39faacf3b5ecf07e22801cafc60c3
tdouce <travisdouce@gmail.com>
2014-11-13 16:48:35 -0500

    Use named route instead of string literal

--- spec/features/creating_models_remotely_spec.rb
+++ spec/features/creating_models_remotely_spec.rb
@@ -10,7 +10,7 @@ describe 'Creating models remotely' do
   end
 
   it 'should return a JSON response' do
-    post "/remote_factory_girl_home_rails/home", {'factory' => 'user', 'attributes' => {'first_name' => 'Sam'}}
+    post remote_factory_girl_home_rails.home_index_path, {'factory' => 'user', 'attributes' => {'first_name' => 'Sam'}}
     expect(response_json(response.body)['first_name']).to eq('Sam')
   end
 end
