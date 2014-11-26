f79b82159e08713a870c5ff1122c84b323cc0878
Seiei Higa <hanachin@gmail.com>
2014-11-27 02:52:46 +0900

    use be matcher for rspec 3.1

--- spec/models/remote_factory_girl_home_rails_spec.rb
+++ spec/models/remote_factory_girl_home_rails_spec.rb
@@ -5,13 +5,13 @@ describe RemoteFactoryGirlHomeRails do
   describe '.enable!' do
     it 'should return true when enabled' do
       RemoteFactoryGirlHomeRails.enable!
-      expect(RemoteFactoryGirlHomeRails.enabled?).to be_true
+      expect(RemoteFactoryGirlHomeRails.enabled?).to be true
     end
   end
 
   describe '.enabled?' do
     it 'should return false when disabled' do
-      expect(RemoteFactoryGirlHomeRails.enabled?).to be_false
+      expect(RemoteFactoryGirlHomeRails.enabled?).to be false
     end
   end
 
@@ -19,7 +19,7 @@ describe RemoteFactoryGirlHomeRails do
     it 'should disable' do
       RemoteFactoryGirlHomeRails.enable!
       RemoteFactoryGirlHomeRails.disable!
-      expect(RemoteFactoryGirlHomeRails.enabled?).to be_false
+      expect(RemoteFactoryGirlHomeRails.enabled?).to be false
     end
   end
 end
